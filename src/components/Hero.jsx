d20ebb225c11a605053c409957d4a850a5655fe4
Zain Ahmad Bodla <48013172+zain-bodlah@users.noreply.github.com>
2023-06-04 13:15:13 +0200

    made minor modifications

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,5 @@
+import logo from "../assets/images/logo.jpg";
+
 function Hero() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 pt-10 items-center">
@@ -28,7 +30,7 @@ function Hero() {
       <div className="flex justify-center">
         <div className="h-[320px] w-[250px] p-2 rounded-[20px] bg-gradient-to-b from-[#FF004D]  to-[#2A26B8]">
           <img
-            src="https://media.istockphoto.com/id/1167772639/photo/handsome-man-smiling-cheerful-with-a-big-smile-on-face-showing-teeth-positive-and-happy.jpg?s=170667a&w=0&k=20&c=x0y3nZ8G7mSOg5LYx_GWOxnzijCBLGj8h_aGIRe16fk="
+            src={logo}
             className="h-full w-[250px] object-cover rounded-[20px]"
           />
         </div>
